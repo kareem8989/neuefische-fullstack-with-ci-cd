7dd76ce9c84f4ac0bebb9ab3526d378e64e2ddb3
kareem89 <muslimkareem89@icloud.com>
2023-01-11 10:26:13 +0100

    Add RequestMapping

--- backend/src/main/java/de/neuefische/backend/CarController.java
+++ backend/src/main/java/de/neuefische/backend/CarController.java
@@ -3,9 +3,11 @@ package de.neuefische.backend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/db")
 @RequiredArgsConstructor
 public class CarController {
 
